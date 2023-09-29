501507e560dd4c355e6c0319963c3fada55444b5
Manish Sharma <sharmakmanish1@gmail.com>
2023-09-29 21:11:57 +0530

    navbar

--- src/Admin/AdminMain/AdminMain.jsx
+++ src/Admin/AdminMain/AdminMain.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import "./AdminMain.scss";
+import { Navbar } from "../components/Navbar/Navbar";
 export const AdminMain = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
+        <Navbar />
         <Outlet />
       </div>
     </div>
